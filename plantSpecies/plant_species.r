bb5259e4344d89d83aba819466dfa9ca3496483a
Allen Hurlbert <hurlbert@bio.unc.edu>
2021-05-24 22:46:23 -0400

    plant species lists

--- plantSpecies/plant_species.r
+++ plantSpecies/plant_species.r
@@ -9,57 +9,71 @@ plants = read.csv(list.files()[grep('Plant.csv', list.files())], header = TRUE, 
 plantspp = data.frame(plantName = unique(plants$Species)) %>%
   mutate(plantName = as.character(plantName),
          cleanedPlantName = case_when(
-           plantName == "Hydrangeas" ~ "Hydrangea",
+           plantName == "American witch-hazel" ~ "witch-hazel",
+           plantName == "American yellowwood" ~ "Kentucky yellowwood",
+           plantName == "Arrowwood" ~ "Viburnum",
+           plantName == "Arrowwood viburnum" ~ "Viburnum",
+           plantName == "Big-leaf dogwood" ~ "Cornus macrophylla",
            plantName == "Blueberry vaccinium sp." ~ "Vaccinium",
            plantName == "Box elder (acer negundo)" ~ "Acer negundo",
            plantName == "Boxelder maple" ~ "Acer negundo",
-           plantName == "American witch-hazel" ~ "witch-hazel",
            plantName == "Burr oak" ~ "Bur oak",
            plantName == "Bush honeysuckle (caprifoliaceae family)" ~ "Bush honeysuckle",
            plantName == "California-laurel" ~ "California laurel",
            plantName == "Crab apple sp" ~ "Malus",
+           plantName == "Crepe myrtle" ~ "Crapemyrtle",
            plantName == "Eastern sweet shrub" ~ "Eastern sweetshrub",
+           plantName == "Fraser magnolia" ~ "Fraser's magnolia",
+           plantName == "Mountain or fraser magnolia" ~ "Fraser's magnolia",
+           plantName == "Honey suckle" ~ "Honeysuckle",
            plantName == "Hop hornbeam" ~ "Hophornbeam",
            plantName == "Hop-hornbeam" ~ "Hophornbeam",
+           plantName == "Hydrangeas" ~ "Hydrangea",
            plantName == "Pear tree" ~ "Pear",
+           plantName == "Red bay" ~ "Redbay",
            plantName == "Red osier dogwood" ~ "Redosier dogwood",
            plantName == "Red-osier dogwood" ~ "Redosier dogwood",
            plantName == "Red osier dogwood (cornus sericea)" ~ "Redosier dogwood",
            plantName == "Sweet gum" ~ "Sweetgum",
            plantName == "Simplocos tinctoria" ~ "Symplocos tinctoria",
            plantName == "Symplocos tinctotria" ~ "Symplocos tinctoria",
+           plantName == "Trembling aspen" ~ "Quaking aspen",
+           plantName == "Vaccinum corymbosum" ~ "Vaccinium corymbosum",
            plantName == "Virburnum" ~ "Viburnum",
+           plantName == "Wild cherry" ~ "Prunus",
            plantName %in% c("", "8", "9", "N/A", "Unknown") ~ "NA",
            TRUE ~ plantName),
          cleanedPlantName = str_replace(cleanedPlantName, " spp.$", ""),
          cleanedPlantName = str_replace(cleanedPlantName, " spp$", ""),
          cleanedPlantName = str_replace(cleanedPlantName, " sp$", ""),
          cleanedPlantName = str_replace(cleanedPlantName, " sp.$", ""),
-         cleanedPlantName = str_replace(cleanedPlantName, " species$", "")
+         cleanedPlantName = str_replace(cleanedPlantName, " species$", ""),
+         cleanedPlantName = str_replace(cleanedPlantName, "?", "")
          ) 
 
 
 # Finding matching taxonomic entity in ITIS
 plantList = data.frame(cleanedName = unique(plantspp$cleanedPlantName[plantspp$cleanedPlantName != "NA"]), 
-                          sciName = NA, 
-                          itis_id = NA)
+                       sciName = NA, 
+                       itis_id = NA,    
+                       rank = NA)
 
 for (i in 1:nrow(plantList)) {
   
-  print(paste(i, "of", nrow(plantList), "\n"))
+  print(paste(i, "of", nrow(plantList), "/n"))
   
   hierarchy = classification(plantList$cleanedName[i], db = 'itis', accepted = TRUE)[[1]]
   
   # class is logical if taxonomic name does not match any existing names
   if (!is.null(nrow(hierarchy))) {
-    if ('species' %in% hierarchy$rank) {
-      plantList$sciName[i] = hierarchy$name[hierarchy$rank == 'species']
-      plantList$itis_id[i] = hierarchy$id[hierarchy$rank == 'species']
-    } else if ('genus' == hierarchy$rank[nrow(hierarchy)]) {
-      plantspp$sciName[i] = paste(hierarchy$name[hierarchy$rank == 'genus'], 'spp.')
-      plantspp$itis_id[i] = hierarchy$id[hierarchy$rank == 'genus']
-    }
-  }
+    plantList$sciName[i] = hierarchy$name[nrow(hierarchy)]
+    plantList$itis_id[i] = hierarchy$id[nrow(hierarchy)]
+    plantList$rank = hierarchy$rank[nrow(hierarchy)]
+  } else {
+    plantList$sciName[i] = NA
+    plantList$itis_id[i] = NA
+    plantList$rank[i] = NA
+  }    
 }
 
 
@@ -104,3 +118,7 @@ write.table(allplants, 'z:/projects/caterpillarscount/fia by state/fia_plus_cc_p
 
 #fia = read.table('z:/projects/caterpillarscount/FIA by state/tree_ids.txt', header= T, sep = '\t')
 #fia$lower = tolower(fia$commonName)
+
+fiacc = read.csv('plantSpecies/FIA_CC_plantList.csv', header = T, quote = '\"')
+uniquePlantList = fiacc %>%
+  distinct(commonName, sciName)

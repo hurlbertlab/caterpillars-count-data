d12c6163239bd87bd5424754c6927649f57ab9dc
Allen Hurlbert <hurlbert@bio.unc.edu>
2022-01-26 18:47:24 -0500

    separating arth length vs quantity flags, modifying flag thresholds

--- dataCleaning/reading_and_cleaning_functions.r
+++ dataCleaning/reading_and_cleaning_functions.r
@@ -62,23 +62,42 @@ qaqc = function(dataToCheck,               # subset of fullDataset dataframe (i.
 ) {
   
   arthQAQC = dataToCheck %>%
-    mutate(arthFlag = case_when(
-      Group == 'ant' & Length > 15 | Group == 'ant' & Quantity > 50 ~ 'ant',
-      Group == 'aphid' & Length > 10 | Group == 'aphid' & Quantity > 50 ~ 'aphid',
-      Group == 'bee' & Length > 25 | Group == 'bee' & Quantity > 6 ~ 'bee',
-      Group == 'beetle' & Length > 20 | Group == 'beetle' & Quantity > 6 ~ 'beetle',
-      Group == 'caterpillar' & Length > 50 | Group == 'caterpillar' & Quantity > 6 ~ 'caterpillar',
-      Group == 'daddylonglegs' & Length > 15 | Group == 'daddylonglegs' & Quantity > 6 ~ 'daddylonglegs',
-      Group == 'fly' & Length > 15 | Group == 'fly' & Quantity > 6 ~ 'fly',
-      Group == 'grasshopper' & Length > 20 | Group == 'grasshopper' & Quantity > 6 ~ 'grasshopper',
-      Group == 'leafhopper' & Length > 20 | Group == 'leafhopper' & Quantity > 6 ~ 'leafhopper',
-      Group == 'moths' & Length > 30 | Group == 'moths' & Quantity > 6 ~ 'moths',
-      Group == 'other' & Length > 25 | Group == 'other' & Quantity > 6 ~ 'other',
-      Group == 'spider' & Length > 15 | Group == 'spider' & Quantity > 6 ~ 'spider',
-      Group == 'truebugs' & Length > 25 | Group == 'truebugs' & Quantity > 6 ~ 'truebugs',
-      Group == 'unidentified' & Length > 25 | Group == 'unidentified' & Quantity > 6 ~ 'unidentified',
-      TRUE ~ ''
-    ))
+    mutate(arthFlagLength = case_when(
+      Group == 'ant' & Length > 17 ~ 'antLength',
+      Group == 'aphid' & Length > 10 ~ 'aphidLength',
+      Group == 'bee' & Length > 25 ~ 'beeLength',
+      Sawfly == 1 & Length > 50 ~ 'sawflyLength',
+      Group == 'beetle' & Length > 20 ~ 'beetleLength',
+      Group == 'caterpillar' & Length > 50 ~ 'caterpillarLength',
+      Group == 'daddylonglegs' & Length > 15 ~ 'daddylonglegsLength',
+      Group == 'fly' & Length > 20 ~ 'flyLength',
+      Group == 'grasshopper' & Length > 20 ~ 'grasshopperLength',
+      Group == 'leafhopper' & Length > 20 ~ 'leafhopperLength',
+      Group == 'moths' & Length > 30 ~ 'mothLength',
+      Group == 'other' & Length > 25 ~ 'otherLength',
+      Group == 'spider' & Length > 20 ~ 'spiderLength',
+      Group == 'truebugs' & Length > 25 ~ 'truebugLength',
+      Group == 'unidentified' & Length > 25 ~ 'unidLength',
+      TRUE ~ ''),
+      arthFlagNum = case_when(
+        Group == 'ant' & Quantity > 50 ~ 'antNum',
+        Group == 'aphid' & Quantity > 50 ~ 'aphidNum',
+        Group == 'bee' & Quantity > 6 ~ 'beeNum',
+        Sawfly == 1 & Quantity > 20 ~ 'sawflyNum',
+        Group == 'beetle' & Quantity > 10 ~ 'beetleNum',
+        Group == 'caterpillar' & Quantity > 6 ~ 'caterpillarNum',
+        Group == 'daddylonglegs' & Quantity > 6 ~ 'daddylonglegsNum',
+        Group == 'fly' & Quantity > 6 ~ 'flyNum',
+        Group == 'grasshopper' & Quantity > 6 ~ 'grasshopperNum',
+        Group == 'leafhopper' & Quantity > 6 ~ 'leafhopperNum',
+        Group == 'moths' & Quantity > 6 ~ 'mothNum',
+        Group == 'other' & Quantity > 6 ~ 'otherNum',
+        Group == 'spider' & Quantity > 6 ~ 'spiderNum',
+        Group == 'truebugs' & Quantity > 6 ~ 'truebugNum',
+        Group == 'unidentified' & Quantity > 6 ~ 'unidNum',
+        TRUE ~ ''
+      )
+    )
     
   survQAQC = dataToCheck %>%
     group_by(ID, NumberOfLeaves, AverageLeafLength) %>%
@@ -90,12 +109,14 @@ qaqc = function(dataToCheck,               # subset of fullDataset dataframe (i.
            survFlag2 = ifelse(totalArthDiv > arthDiversityMax, paste(survFlag1, 'totalArthDiv'), survFlag1),
            survFlag3 = ifelse(NumberOfLeaves > numberLeavesMax | NumberOfLeaves < numberLeavesMin, paste(survFlag2, 'numLeaves'), survFlag2),
            survFlag4 = ifelse(AverageLeafLength > leafLengthMax, paste(survFlag3, 'leafLength'), survFlag3),
-           survFlags = trimws(survFlag4)) %>%
-    dplyr::select(ID, survFlags) 
+           survFlag5 = trimws(survFlag4)) %>%
+    dplyr::select(ID, survFlag5) 
   
   finalQAQC = arthQAQC %>%
     left_join(survQAQC, by = 'ID') %>%
-    mutate(flags = trimws(paste(arthFlag, survFlags)),
+    # if there is an arthropod flag for abundance, then no need to report the totalArthAbund flag
+    mutate(survFlags = ifelse(arthFlagNum != "", trimws(gsub('totalArthAbund', '', survFlag5)), survFlag5),
+           flags = trimws(paste(arthFlagNum, arthFlagLength, survFlags)),
            status = ifelse(flags == '', 'ok', 'check'),
            actionTaken = NA) %>%
     dplyr::select(ID:cell, flags, status, actionTaken)
